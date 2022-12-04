7041da79ce3425df8f675e850cb2b55d4e8f327d
AnTomS <thekeibee@yandex.ru>
2022-12-04 23:05:42 +0800

    Добавлены верхний и нижний панели с кнопками

--- app/src/main/java/com/example/instagram/MainActivity.kt
+++ app/src/main/java/com/example/instagram/MainActivity.kt
@@ -1,60 +1,70 @@
 package com.example.instagram
 
-import android.R
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
-import androidx.annotation.NonNull
+
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.ui.AppBarConfiguration
+import androidx.fragment.app.Fragment
 import com.example.instagram.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
-@Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var binding: ActivityMainBinding
 
-//    private val mOnNavigationItemSelectedListener =
-//        BottomNavigationView.OnNavigationItemSelectedListener { item ->
-//            when (item.) {
-//                R.id.home -> {
-//                    loadFragment(HomeFragment.newInstance())
-//                    return@OnNavigationItemSelectedListener true
-//                }
-//                R.id.search -> {
-//                    loadFragment(DashboardFragment.newInstance())
-//                    return@OnNavigationItemSelectedListener true
-//                }
-//                R.id.navigation_notifications -> {
-//                    loadFragment(NotificationsFragment.newInstance())
-//                    return@OnNavigationItemSelectedListener true
-//                }
-//            }
-//            false
-//        }
-
-
+    lateinit var binding: ActivityMainBinding
+    lateinit var bottomNav: BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        loadFragment(HomeFragment())
 
-        setSupportActionBar(binding.topToolbar)
+        binding.bottomNav.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.ic_home -> {
+                    loadFragment(HomeFragment())
+
+                }
+                R.id.ic_search -> {
+                    loadFragment(SearchFragment())
+
+                }
+                R.id.ic_share -> {
+                    loadFragment(ShareFragment())
+
+                }
+                R.id.ic_likes -> {
+                    loadFragment(LikedFragment())
+
+                }
+                R.id.ic_profile -> {
+                    loadFragment(ProfileFragment())
+
+                }
+
+            }
+            true
+        }
 
     }
-//    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        menuInflater.inflate(R.menu.menu_main, menu)
-//        return true
-//    }
+
+    private fun loadFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+    }
 }
 
-//
-//
+
+//    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        menuInflater.inflate(R.menu., menu)
+//        return true
+//    }
+
+
 //        val navController = findNavController(R.id.nav_host_fragment_content_main)
 //        appBarConfiguration = AppBarConfiguration(navController.graph)
 //        setupActionBarWithNavController(navController, appBarConfiguration)
@@ -65,13 +75,13 @@ class MainActivity : AppCompatActivity() {
 //                .setAction("Action", null).show()
 //        }
 //    }
-
 //
-
+//
+//
 //    override fun onOptionsItemSelected(item: MenuItem): Boolean {
 //        // Handle action bar item clicks here. The action bar will
 //        // automatically handle clicks on the Home/Up button, so long
-//        // as you specify a parent activity in AndroidManifest.xml.
+//       // as you specify a parent activity in AndroidManifest.xml.
 //        return when (item.itemId) {
 //            R.id.action_settings -> true
 //            else -> super.onOptionsItemSelected(item)
