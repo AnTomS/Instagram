250dffa179e32ee4a5dfe8a48f81ea49b2c67600
AnTomS <thekeibee@yandex.ru>
2022-12-11 15:02:58 +0800

    Добавлен Нав фрагмент, подключен переход на фрагменты из активити

--- app/src/main/java/com/example/instagram/MainActivity.kt
+++ app/src/main/java/com/example/instagram/MainActivity.kt
@@ -5,8 +5,14 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.instagram.databinding.ActivityMainBinding
 import com.example.instagram.fragment.*
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -14,10 +20,9 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var navController: NavController
     lateinit var binding: ActivityMainBinding
-    lateinit var bottomNav: BottomNavigationView
-
-    private val host by lazy { NavHostFragment.create(R.navigation.nav_graph) }
+    private lateinit var bottomNav: BottomNavigationView
 
 
 //    override fun onCreate(savedInstanceState: Bundle?) {
@@ -47,7 +52,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
-        //setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main)
+
+
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -57,10 +64,11 @@ class MainActivity : AppCompatActivity() {
 
         loadFragment(HomeFragment.newInstance())
 
+
         binding.bottomNav.setOnItemSelectedListener { item ->
-            var fragment: Fragment
+            val fragment: Fragment
             when (item.itemId) {
-                R.id.ic_home -> {
+                R.id.ic_home-> {
                     toolbar?.setTitle("Home")
                     fragment = HomeFragment()
                     loadFragment(fragment)
@@ -101,19 +109,19 @@ class MainActivity : AppCompatActivity() {
 
         binding.bottomNav.setOnItemReselectedListener { item ->
             when (item.itemId) {
-                R.id.ic_home -> {
+                R.id.homeFragment -> {
                     Toast.makeText(this, "Home Item reselected", Toast.LENGTH_SHORT).show()
                 }
-                R.id.ic_search -> {
+                R.id.searchFragment -> {
                     Toast.makeText(this, "Search Item reselected", Toast.LENGTH_SHORT).show()
                 }
-                R.id.ic_share -> {
+                R.id.shareFragment-> {
                     Toast.makeText(this, "Share Item reselected", Toast.LENGTH_SHORT).show()
                 }
-                R.id.ic_likes -> {
+                R.id.likedFragment -> {
                     Toast.makeText(this, "My liked Item reselected", Toast.LENGTH_SHORT).show()
                 }
-                R.id.ic_profile -> {
+                R.id.profileFragment -> {
                     Toast.makeText(this, "My profile Item reselected", Toast.LENGTH_SHORT).show()
 
                 }
@@ -124,7 +132,7 @@ class MainActivity : AppCompatActivity() {
     private fun loadFragment(fragment: Fragment) {
         // load fragment
         supportFragmentManager.beginTransaction()
-            .replace(R.id.container, fragment)
+            .replace(R.id.nav_host_fragment, fragment)
             .commit()
     }
 }
