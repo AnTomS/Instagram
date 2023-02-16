91c4fb258d775d896242d45625f5fd0dae1aef74
AnTomS <thekeibee@yandex.ru>
2023-02-17 00:41:15 +0800

    Добавление вёрстки экрана профиля + добавление обработчика "редактирование профиля"

--- app/src/main/java/com/example/instagram/fragment/ProfileFragment.kt
+++ app/src/main/java/com/example/instagram/fragment/ProfileFragment.kt
@@ -5,22 +5,35 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.appcompat.widget.AppCompatButton
+import androidx.navigation.fragment.findNavController
 import com.example.instagram.R
+import com.example.instagram.databinding.ActivityMainBinding
 
 class ProfileFragment : Fragment() {
+
+    private lateinit var button: Button
+
+    lateinit var binding: ActivityMainBinding
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? =
-        inflater.inflate(R.layout.fragment_profile, container, false)
+        savedInstanceState: Bundle?,
+    ): View {
+        val view = inflater.inflate(R.layout.fragment_profile, container, false)
 
-    companion object {
+        button = view.findViewById(R.id.edit_profile)
 
-        fun newInstance() = ProfileFragment()
+
+        return view
     }
 
-override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-    super.onViewCreated(view, savedInstanceState)
-}
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        button.setOnClickListener {
+            findNavController().navigate(R.id.editProfileFragment)
+        }
+    }
 }
