b36c9ff9b931324589cb838c945e835a00c377d9
AnTomS <thekeibee@yandex.ru>
2023-04-12 22:06:07 +0800

    Добавлено окно регистрации + рефакторинг окна логина

--- app/src/main/java/com/example/instagram/fragment/RegisterFragment.kt
+++ app/src/main/java/com/example/instagram/fragment/RegisterFragment.kt
@@ -1,10 +1,10 @@
 package com.example.instagram.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.example.instagram.R
 
 
@@ -21,7 +21,12 @@ class RegisterFragment : Fragment() {
         savedInstanceState: Bundle?,
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false)
+        return inflater.inflate(R.layout.fragment_register_mail, container, false)
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
     }
 
 
