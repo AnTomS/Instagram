d4f22f403a8325e59aa714a81c2f1d444b64a399
AnTomS <thekeibee@yandex.ru>
2023-04-16 21:55:33 +0800

    Добавлено окно регистрации + рефакторинг окна логина

--- app/src/main/java/com/example/instagram/fragment/RegisterFragmentMail.kt
+++ app/src/main/java/com/example/instagram/fragment/RegisterFragmentMail.kt
@@ -4,15 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.instagram.R
 import com.example.instagram.databinding.FragmentRegisterMailBinding
+import com.example.instagram.databinding.FragmentRegisterNamepassBinding
 
 
 class RegisterFragmentMail : Fragment() {
-    private val args by navArgs<RegisterFragmentNamePassArgs>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -25,10 +27,14 @@ class RegisterFragmentMail : Fragment() {
         // Inflate the layout for this fragment
         val binding = FragmentRegisterMailBinding.inflate(inflater, container, false)
         binding.nextBtn.setOnClickListener {
-            findNavController().navigate(R.id.action_registerFragmentMail_to_registerFragmentNamePass)
-
             val email = binding.newEmailInput.text.toString()
-        }
+            if (email.isNotEmpty()) {
+                findNavController().navigate(R.id.action_registerFragmentMail_to_registerFragmentNamePass)
+            } else {
+                Toast.makeText(requireActivity(), "Введите Ваш адрес электронной почты", Toast.LENGTH_SHORT).show()
+            }
+            }
+
 
 
         return binding.root
--- app/src/main/java/com/example/instagram/fragment/RegisterFragmentNamePass.kt
+++ app/src/main/java/com/example/instagram/fragment/RegisterFragmentNamePass.kt
@@ -1,19 +1,28 @@
 package com.example.instagram.fragment
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.instagram.R
 import com.example.instagram.databinding.FragmentRegisterMailBinding
 import com.example.instagram.databinding.FragmentRegisterNamepassBinding
+import com.example.instagram.models.User
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class RegisterFragmentNamePass : Fragment() {
-    private val args by navArgs<RegisterFragmentNamePassArgs>()
 
+
+    private val args by navArgs<RegisterFragmentNamePassArgs>()
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var _DataBase: DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -25,23 +34,66 @@ class RegisterFragmentNamePass : Fragment() {
     ): View? {
         val binding = FragmentRegisterNamepassBinding.inflate(inflater, container, false)
         binding.registerBtn.setOnClickListener {
-            findNavController()
-
-            val email = args.
-            val name = binding.newFullNameInput.text.toString()
+            val email = args.myArg
+            val fullName = binding.newFullNameInput.text.toString()
             val password = binding.addPassword.text.toString()
+            onRegister(fullName, password, email)
         }
 
-
         return binding.root
-
-
     }
 
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    private fun onRegister(fullName: String, password: String, email: String) {
+        firebaseAuth = FirebaseAuth.getInstance()
+        _DataBase = FirebaseDatabase.getInstance().getReference("users")
+        if (fullName.isNotEmpty() && password.isNotEmpty()) {
+            if (email != null) {
+                firebaseAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener {
+                        if (it.isSuccessful) {
+                            val user = makeUser(fullName, email)
+                            _DataBase.child("users").child(it.result.user!!.uid).setValue(user)
+                            if (it.isSuccessful) {
+                                findNavController().navigate(R.id.homeFragment)
+                            } else {
+                                Log.e("Tag", "failed to create user profile", it.exception)
+                                Toast.makeText(
+                                    requireActivity(),
+                                    "Something went wrong. Try again later",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
+                        } else {
+                            Log.e("Tag", "onRegister: Email is null")
+                            Toast.makeText(
+                                requireActivity(),
+                                "Please enter email",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                            findNavController().popBackStack()
 
+                        }
+                    }
+            } else {
+                Toast.makeText(
+                    requireActivity(),
+                    "Please enter Full Name and Password",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+    }
+
+    private fun username(fullName: String): String {
+        return fullName.lowercase().replace(" ", ".")
+    }
+
+    private fun makeUser(fullName: String, email: String): User {
+        val username = username(fullName)
+        return User(name = fullName, username = username, email = email)
     }
 
 
 }
+
