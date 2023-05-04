bde1fd642484f71e3aecc6fdcaeca215672813f0
AnTomS <thekeibee@yandex.ru>
2023-05-04 08:35:01 +0800

    рефакторинг окна регистрации

--- app/src/main/java/com/example/instagram/fragment/RegisterFragmentMail.kt
+++ app/src/main/java/com/example/instagram/fragment/RegisterFragmentMail.kt
@@ -7,10 +7,7 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import androidx.navigation.fragment.navArgs
-import com.example.instagram.R
 import com.example.instagram.databinding.FragmentRegisterMailBinding
-import com.example.instagram.databinding.FragmentRegisterNamepassBinding
 import com.google.firebase.auth.FirebaseAuth
 
 
